9f2caac64d496a021eff95f90fad8f19d56b9a6b
IBIRASA <d.ishimwe@alustudent.com>
2024-08-01 21:04:17 +0200

    updated

---
+++ sheconnects/src/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import Login from './Pages/Login.jsx';
+import Register from './Pages/Register.jsx';
+import MainCommunity from './Pages/MainCommunity.jsx';
+import Dashboard from './Pages/Dashboard.jsx';
+import Profiledit from './Pages/Profiledit.jsx';
+import CommunityTop from './components/Community_top.jsx';
+import Business from './components/Business.jsx';
+import IT from './components/IT.jsx';
+import Channels from './components/Channels.jsx';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+const Root = () => {
+  const username = localStorage.getItem('username') || 'Guest';
+  console.log("Username:", username); 
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/MainCommunity" element={<MainCommunity />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/Profiledit" element={<Profiledit />} />
+        <Route path="/business" element={<Business />} />
+        <Route path="/it" element={<IT />} />
+        <Route path="/channels" element={<Channels />} />
+        <Route path="/Community_top" element={<CommunityTop username={username} />} />
+
+      </Routes>
+    </Router>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
